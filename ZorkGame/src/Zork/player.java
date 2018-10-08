4c67aec7b25e7d0288eb7a02bfe5467408c3a621
jzzhang <jzzhang@PLD-LAB-400-13T.fayette.ketsds.net>
2018-10-08 10:39:15 -0400

    Began working on inventory

--- ZorkGame/src/Zork/player.java
+++ ZorkGame/src/Zork/player.java
@@ -6,6 +6,20 @@ public class player {
 		inventory = new items[12];
 	}
 	
+	public static void pickUpItem(items i) {
+		if (calcInvWeight() < 100) {
+			
+		}
+	}
+	
+	private static long calcInvWeight() {
+		long currentWeight = 0;
+		for (items i : inventory) {
+			currentWeight += i.weight;
+		}
+		return currentWeight;
+	}
+	
 	public static items[] inventory;
 	
 
